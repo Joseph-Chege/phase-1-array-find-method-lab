e36bcdd85d625e4acec998d61f5e6bdadac34c3b
Joseph-Chege <mjchege23@gmail.com>
2024-04-09 12:40:35 +0300

    Created a function and found an element in the object using .find()

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(records){
+    const winRecords = records.find(record => record.result === "W");
+    return winRecords ? winRecords.year : undefined;
+
+   
+}
